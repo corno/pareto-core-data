c7c1cf5eed4e13324d1a0faa9aa2be6f0f7e1c14
Corno Schraverus <corno@schraverus.com>
2024-09-03 11:31:46 +0200

    .

--- build/scripts/publish.sh
+++ build/scripts/publish.sh
@@ -9,14 +9,13 @@ if [ -z "$1" ]
     exit 1
 fi
 
-#the scriptsdir will be deleted, change to the root
-cd "$rootDir" && \
-
 #make sure everything is pushed
 git push && \
 
 #validate that everything is committed and pushed (to make sure we're not messing with open work)
 git diff --exit-code && git log origin/master..master --exit-code && \
+
+node $scriptDir/../node_modules/npm-updatedependencies2latest/dist/index.js $rootDir/pub dependencies && \
 
 $scriptDir/build.sh && \
 
